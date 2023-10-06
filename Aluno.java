7a9cb7f3beb41455bb5d3e229c3c1a51b82c4a13
MariaPacheco2001 <patcheco.f1@gmail.com>
2023-10-06 10:30:35 +0100

    Class Aluno - versao 1

---
+++ Aluno.java
@@ -0,0 +1,28 @@
+public class Aluno {
+
+    private String nome;
+    private String matricula;
+    private double saldo;
+
+    public Aluno(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.saldo = 0.0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void adicionarSaldo(double valor) {
+        saldo += valor;
+    }
+}
