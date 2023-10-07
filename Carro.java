2f38e02554d7178863c83f3703b5b22d5269d206
joaoalves14 <joaoalves96@icloud.com>
2023-10-07 19:27:35 +0100

    Class Carro - Versão 1

---
+++ Carro.java
@@ -0,0 +1,11 @@
+public class Carro {
+    private String matricula;
+
+    public Carro(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+}
